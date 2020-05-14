7f4747e29898a899b2ca278e4e160e4a9397ad9a
Omar <51249360+omargg83@users.noreply.github.com>
2020-05-13 21:45:54 -0500

    a
    a

--- dash/menu.php
+++ dash/menu.php
@@ -5,11 +5,11 @@
         <a href='#dash/index' class='activeside'><i class='fas fa-home'></i><span>Inicio</span></a>
         <a href='#a_ventas/index' title='Pedidos'><i class='fas fa-shopping-basket'></i><span>Ventas</span></a>
         <a href='#a_productos/index' title='Productos'><i class='fab fa-product-hunt'></i><span>Productos</span></a>
+        <a href='#a_citas/index' title='Citas'><i class='fas fa-user-tag'></i><span>Citas</span></a>
 <!--        <a href='#a_reportes/index' title='Reportes'><i class="fas fa-database"></i><span>Reportes</span></a>  -->
         <hr>
         <a href='#a_usuarios/index' title='Usuarios'><i class='fas fa-users'></i> <span>Usuarios</span></a>
         <a href='#a_cliente/index' title='Clientes'><i class='fas fa-user-tag'></i><span>Clientes</span></a>
-
         <!--
         <a href='#a_tienda/index' title='sucursal'><i class="fas fa-store-alt"></i><span>Sucursal</span></a>
         <a href='#a_asistencia/index' title='Asistencia'><i class='fas fa-user-check'></i><span>Asistencia</span></a>
