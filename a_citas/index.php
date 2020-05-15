c4a2ddb8c2e5b917689ff78d99b7e645b1d7587d
Omar <51249360+omargg83@users.noreply.github.com>
2020-05-15 14:20:08 -0500

    a
    a

--- a_citas/index.php
+++ a_citas/index.php
@@ -1,47 +1,52 @@
 <?php
-	require_once("db_.php");
+  require_once("db_.php");
 
-	echo "<nav class='navbar navbar-expand-lg navbar-light bg-light'>
+ ?>
 
-	<a class='navbar-brand' ><i class='fas fa-users'></i> Citas</a>
+ <nav class='navbar navbar-expand-sm navbar-light bg-light'>
+ 		  <a class='navbar-brand' ><i class="fas fa-shopping-basket"></i>Pedidos</a>
+ 		  <button class='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+ 			<span class='navbar-toggler-icon'></span>
+ 		  </button>
+ 		  <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+ 			<ul class='navbar-nav mr-auto'>
+        <div class='form-inline my-2 my-lg-0' id='daigual' action='' >
+          <div class="input-group  mr-sm-2">
+            <input type="text" class="form-control form-control-sm" placeholder="Buscar" aria-label="Buscar" aria-describedby="basic-addon2"  id='buscar' onkeyup='Javascript: if (event.keyCode==13) buscar_pedido()'>
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary btn-sm" type="button" onclick='buscar_pedido()'><i class='fas fa-search'></i></button>
+            </div>
+          </div>
+				</div>
+        <li class='nav-item active'><a class='nav-link barranav' title='Mostrar todo' id='calendario' onclick='calendar_load(1)'><i class='fas fa-list-ul'></i><span>Calendario</span></a></li>
+ 				<li class='nav-item active'><a class='nav-link barranav' title='Mostrar todo' id='new_poliza' data-lugar='a_citas/editar'><i class="fas fa-folder-plus"></i><span>Nuevo</span></a></li>
+ 				<li class='nav-item active'><a class='nav-link barranav' title='Mostrar todo' id='lista_prod' data-lugar='a_citas/lista'><i class="fas fa-list"></i><span>Lista</span></a></li>
 
-	  <button class='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='principal' aria-expanded='false' aria-label='Toggle navigation'>
-		<span class='navbar-toggler-icon'></span>
-	  </button>
-		  <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-			<ul class='navbar-nav mr-auto'>";
+      </li>
 
-			echo "<li class='nav-item active'><a class='nav-link barranav' title='Mostrar todo' id='calendario' onclick='calendar_load(1)'><i class='fas fa-list-ul'></i><span>Calendario</span></a></li>";
+ 			</ul>
+ 		</div>
+ 	  </div>
+ 	</nav>
 
-      echo "<li class='nav-item active'><a class='nav-link barranav' title='Mostrar todo' id='winmodal_pass' data-id='0' data-lugar='a_citas/editar_cita'><i class='fas fa-list-ul'></i><span>Nuevo</span></a></li>";
+<?php
 
+   echo "<div id='trabajo' style='margin-top:5px;'>";
+    include 'lista.php';
+   echo "</div>";
 
-			echo "</ul>";
-
-			echo "<form class='form-inline my-2 my-lg-0' id='consulta_avanzada' action='' data-destino='a_personal/lista' data-funcion='guardar' data-div='trabajo'>
-			<input class='form-control mr-sm-2' type='search' placeholder='Busqueda global' aria-label='Search' name='valor' id='valor'>
-			<div class='btn-group'>
-			<button class='btn btn-outline-secondary btn-sm' type='submit' title='Buscar' ><i class='fas fa-search'></i></button>
-			</div>
-			</form>";
-		echo "
-	  </div>
-	</nav>";
-
-	echo "<div class='container' style='background-color:".$_SESSION['cfondo']."; '>";
-	echo "<div id='trabajo' class='container'>";
-
-  echo "</div>";
-  echo "</div>";
-?>
-<script>
+ ?>
+<script type="text/javascript">
 
 $(function(){
-	calendar_load(1);
+  calendar_load(1);
 });
 
 function calendar_load(tipo){
   var fecha = new Date();
+
+  $('#trabajo').html("");
+
   var calendarEl = document.getElementById('trabajo');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -86,4 +91,380 @@ function calendar_load(tipo){
   });
   calendar.render();
 }
+
+  function buscar_pedido(){
+    var buscar = $("#buscar").val();
+    $.ajax({
+      data:  {
+        "buscar":buscar
+      },
+      url:   'a_citas/lista.php',
+      type:  'post',
+      success:  function (response) {
+        $("#trabajo").html(response);
+      }
+    });
+  }
+  function buscar_cliente(){
+  	var texto=$("#prod_venta").val();
+  	var idcliente=$("#idcliente").val();
+  	var idcita=$("#idcita").val();
+  	if(texto.length>=-1){
+  		$.ajax({
+  			data:  {
+  				"texto":texto,
+  				"idcliente":idcliente,
+  				"idcita":idcita,
+  				"function":"busca_cliente"
+  			},
+  			url:   "a_citas/db_.php",
+  			type:  'post',
+  			beforeSend: function () {
+  				$("#resultadosx").html("buscando...");
+  			},
+  			success:  function (response) {
+  				$("#resultadosx").html(response);
+  				$("#prod_venta").val();
+  			}
+  		});
+  	}
+  }
+  function buscar_prodpedido(){
+  	var texto=$("#prod_venta").val();
+  	var idproducto=$("#idproducto").val();
+  	var idpedido=$("#idpedido").val();
+  	if(texto.length>=-1){
+  		$.ajax({
+  			data:  {
+  				"texto":texto,
+  				"idproducto":idproducto,
+  				"idpedido":idpedido,
+  				"function":"busca_producto"
+  			},
+  			url:   "a_citas/db_.php",
+  			type:  'post',
+  			beforeSend: function () {
+  				$("#resultadosx").html("buscando...");
+  			},
+  			success:  function (response) {
+  				$("#resultadosx").html(response);
+  				$("#prod_venta").val();
+  			}
+  		});
+  	}
+  }
+  function cliente_add(idcliente,idcita){
+    $.confirm({
+      title: 'Cliente',
+      content: '¿Desea agregar el cliente seleccionado?',
+      buttons: {
+        Aceptar: function () {
+          $.ajax({
+            data:  {
+              "idcliente":idcliente,
+              "idcita":idcita,
+              "function":"agrega_cliente"
+            },
+            url:   "a_citas/db_.php",
+            type:  'post',
+            success:  function (response) {
+              console.log(response);
+              var datos = JSON.parse(response);
+              if (datos.error==0){
+                $.ajax({
+                  data:  {
+                    "id":datos.id
+                  },
+                  url:   'a_citas/editar.php',
+                  type:  'post',
+                  success:  function (response) {
+                    $("#trabajo").html(response);
+                  }
+                });
+                Swal.fire({
+                  type: 'success',
+                  title: "Se agregó correctamente",
+                  showConfirmButton: false,
+                  timer: 1000
+                });
+                $('#myModal').modal('hide');
+              }
+              else{
+                $.alert(datos.terror);
+              }
+            }
+          });
+        },
+        Cancelar: function () {
+          $.alert('Canceled!');
+        }
+      }
+    });
+  }
+  function prod_add(id,idpedido){
+    var cantidad=$("#cantidad_"+id).val();
+    $.confirm({
+      title: 'Producto',
+      content: '¿Desea agregar el producto seleccionado?',
+      buttons: {
+        Aceptar: function () {
+          $.ajax({
+            data:  {
+              "id":id,
+              "idpedido":idpedido,
+              "cantidad":cantidad,
+              "function":"producto_add"
+            },
+            url:   "a_citas/db_.php",
+            type:  'post',
+            success:  function (response) {
+              console.log(response);
+              var datos = JSON.parse(response);
+              if (datos.error==0){
+                $.ajax({
+                  data:  {
+                    "id":datos.id
+                  },
+                  url:   'a_citas/editar.php',
+                  type:  'post',
+                  success:  function (response) {
+                    $("#trabajo").html(response);
+                  }
+                });
+                Swal.fire({
+                  type: 'success',
+                  title: "Se agregó correctamente",
+                  showConfirmButton: false,
+                  timer: 1000
+                });
+                $('#myModal').modal('hide');
+              }
+              else{
+                $.alert(datos.terror);
+              }
+            }
+          });
+        },
+        Cancelar: function () {
+          $.alert('Canceled!');
+        }
+      }
+    });
+  }
+  function buscar_cupon(){
+    var texto=$("#prod_venta").val();
+  	var idpedido=$("#idpedido").val();
+  	if(texto.length>=-1){
+  		$.ajax({
+  			data:  {
+  				"texto":texto,
+  				"idpedido":idpedido,
+  				"function":"busca_cupon"
+  			},
+  			url:   "a_citas/db_.php",
+  			type:  'post',
+  			beforeSend: function () {
+  				$("#resultadosx").html("buscando...");
+  			},
+  			success:  function (response) {
+  				$("#resultadosx").html(response);
+  				$("#prod_venta").val();
+  			}
+  		});
+  	}
+  }
+  function cupon_agrega(idcupon,idpedido){
+    $.ajax({
+      url:  "a_citas/db_.php",
+      type: "POST",
+      data: {
+        "idcupon":idcupon,
+        "idpedido":idpedido,
+        "function":"cupon_busca"
+      },
+      success: function( response ) {
+        console.log(response);
+        var datos = JSON.parse(response);
+        if (datos.error==0){
+          Swal.fire({
+              type: 'success',
+              title: "Se agregó correctamente",
+              showConfirmButton: false,
+              timer: 1000
+          });
+
+          $.ajax({
+            data:  {
+              "id":idpedido
+            },
+            url:   'a_citas/editar.php',
+            type:  'post',
+            success:  function (response) {
+              $("#trabajo").html(response);
+            }
+          });
+
+        }
+        else{
+          Swal.fire({
+              type: 'error',
+              title: datos.terror,
+              showConfirmButton: false,
+              timer: 1000
+          });
+        }
+      }
+    });
+  }
+  function elimina_cuadmin(id,idpedido){
+    $.confirm({
+        title: 'Cupon',
+        content: '¿Desea eliminar el cupón?',
+        buttons: {
+          Eliminar: function () {
+            $.ajax({
+              data:  {
+                "id":id,
+                "idpedido":idpedido,
+                "function":"elimina_cupon"
+              },
+              url:   'a_citas/db_.php',
+              type:  'post',
+              timeout:3000,
+              beforeSend: function () {
+
+              },
+              success:  function (response) {
+                $("#trabajo").load("a_citas/editar.php?id="+idpedido);
+              },
+              error: function(jqXHR, textStatus, errorThrown) {
+
+              }
+            });
+
+          },
+          Cancelar: function () {
+
+          }
+        }
+      });
+  }
+  function confirmar_web(pedido_web,idpedido){
+    $.confirm({
+      title: 'Cliente',
+      content: '¿Desea confirmar el pedido a CT?',
+      buttons: {
+        Aceptar: function () {
+          $.ajax({
+            data:  {
+              "pedido_web":pedido_web,
+              "idpedido":idpedido,
+              "function":"confirmar_web"
+            },
+            url:   "a_citas/db_.php",
+            type:  'post',
+            success:  function (response) {
+              console.log(response);
+              var datos = JSON.parse(response);
+              if (datos.error==0){
+                $.ajax({
+                  data:  {
+                    "id":idpedido
+                  },
+                  url:   'a_citas/editar.php',
+                  type:  'post',
+                  success:  function (response) {
+                    $("#trabajo").html(response);
+                  }
+                });
+                Swal.fire({
+                  type: 'success',
+                  title: "Se agregó correctamente",
+                  showConfirmButton: false,
+                  timer: 1000
+                });
+                $('#myModal').modal('hide');
+              }
+              else{
+                $.ajax({
+                  data:  {
+                    "id":idpedido
+                  },
+                  url:   'a_citas/editar.php',
+                  type:  'post',
+                  success:  function (response) {
+                    $("#trabajo").html(response);
+                  }
+                });
+                Swal.fire({
+                  type: 'error',
+                  title: datos.terror,
+                  showConfirmButton: false,
+                  timer: 1000
+                });
+              }
+
+            }
+          });
+        },
+        Cancelar: function () {
+          $.alert('Canceled!');
+        }
+      }
+    });
+  }
+  function solicitar_ct(id){
+    $.confirm({
+      title: 'Cliente',
+      content: '¿Desea procesar el pedido?, (envio de productos a CT, descuento de inventario)',
+      buttons: {
+        Aceptar: function () {
+          $.ajax({
+      			data:  {
+      				"id":id,
+      				"function":"pedir_ct"
+      			},
+      			url:   "a_citas/db_.php",
+      			type:  'post',
+      			beforeSend: function () {
+              $("#cargando").addClass("is-active");
+      			},
+      			success:  function (response) {
+              var datos = JSON.parse(response);
+              if (datos.error==0){
+                Swal.fire({
+                  type: 'success',
+                  title: "Pedido procesado correctamente",
+                  showConfirmButton: false,
+                  timer: 1000
+                });
+                $.ajax({
+                  data:  {
+                    "id":id
+                  },
+                  url:   'a_citas/editar.php',
+                  type:  'post',
+                  success:  function (response) {
+                    $("#trabajo").html(response);
+                  }
+                });
+      				}
+              else{
+                Swal.fire({
+                  type: 'error',
+                  title: datos.terror,
+                  showConfirmButton: false,
+                  timer: 1000
+                });
+              }
+              $("#cargando").removeClass("is-active");
+      			}
+      		});
+        },
+        Cancelar: function () {
+
+        }
+      }
+    });
+  }
 </script>
---
+++ a_citasx/db_.php
@@ -0,0 +1,195 @@
+<?php
+require_once("../control_db.php");
+if (isset($_REQUEST['function'])){$function=$_REQUEST['function'];}	else{ $function="";}
+
+class Escritorio extends Sagyc{
+	private $accesox;
+	private $comic;
+	private $editar;
+
+	public function __construct(){
+		parent::__construct();
+	}
+	public function citas_lista($tipo){
+		try{
+			self::set_names();
+			$fecha=date('Y-m-d')." 00:00:00";
+			$sql="select * from citas left outer join afiliados on afiliados.idfolio=citas.idfolio
+			where apartado=2 and tipo='$tipo' and fecha>='$fecha'";
+			$sth = $this->dbh->prepare($sql);
+			$sth->execute();
+			return $sth->fetchAll();
+		}
+		catch(PDOException $e){
+			return "Database access FAILED! ".$e->getMessage();
+		}
+	}
+
+
+
+
+
+	public function citas(){
+		try{
+			$desde=$_REQUEST['desde'];
+			$hora=$_REQUEST['hora'];
+			$minuto=$_REQUEST['minuto'];
+			$tipo=$_REQUEST['tipo'];
+
+			$actual=date('Y-m-d H:i:s');
+			$fechax = date("Y-m-d", strtotime($desde))." $hora:$minuto:00";
+			$limite=new DateTime();
+			$limite->modify("+6 minute");
+
+			$sql="select * from citas where tipo='$tipo' and fecha='$fechax' and (apartado=2 or (apartado=1 and limite>'$actual'))";
+			$sth = $this->dbh->prepare($sql);
+			$sth->execute();
+			$reg=$sth->rowCount();
+
+			if($reg<4){
+				$sql="select * from citas where tipo='$tipo' and fecha='$fechax' and (apartado=1 and limite<'$actual')";
+				$sth = $this->dbh->prepare($sql);
+				$sth->execute();
+
+				$arreglo=array();
+				$arreglo+=array('idfolio'=>$_SESSION['idfolio']);
+				$arreglo+=array('fecha'=>$fechax);
+				$arreglo+=array('caja'=>1);
+				$arreglo+=array('apartado'=>1);
+				$arreglo+=array('limite'=>$limite->format('Y-m-d H:i:s'));
+				$arreglo+=array('fcreado'=>$actual);
+				$arreglo+=array('tipo'=>$tipo);
+
+				if($sth->rowCount()==0){
+					$arreglo+=array('caja'=>1);
+					$x=$this->insert('citas', $arreglo);
+				}
+				else{
+					$tmp=$sth->fetch(PDO::FETCH_OBJ);
+					$x=$this->update('citas',array('id'=>$tmp->id), $arreglo);
+				}
+				$cita=json_decode($x);
+
+				$t="<div class='card'>";
+					$t.="<div class='card-header'>";
+						$t.="Confirmar";
+					$t.="</div>";
+					$t.="<div class='card-body'>";
+					$t.= "<input class='form-control' type='hidden' id='tipo' name='tipo' value='$tipo' readonly>";
+						$t.="<div class='row'>";
+							$t.= "<div class='col-3'>";
+									$t.= "<label># Cita</label>";
+									$t.= "<input class='form-control' type='text' id='cita' name='cita' value='$cita->id' readonly>";
+							$t.= "</div>";
+							$t.= "<div class='col-3'>";
+									$t.= "<label>Fecha</label>";
+									$t.= "<input class='form-control' type='text' id='desde' name='desde' value='$desde' readonly>";
+							$t.= "</div>";
+							$t.= "<div class='col-3'>";
+									$t.= "<label>Hora</label>";
+									$t.= "<input class='form-control' type='text' id='hora' name='hora' value='$hora:$minuto:00' readonly>";
+							$t.= "</div>";
+							$t.= "<div class='col-12'>";
+									$t.= "<label>Observaciones</label>";
+									$t.= "<input class='form-control' type='text' id='observaciones' name='observaciones' value='' placeholder='Observaciones'>";
+							$t.= "</div>";
+						$t.= "</div>";
+						$t.="<div class='row'>";
+							$t.= "<div class='col-12'>";
+								$x.="<div class='btn-group'>";
+									$t.= "<button class='btn btn-warning btn-sm' type='button' onclick='confirmar_cita()'><i class='far fa-calendar-check'></i>Confirmar cita</button>";
+									$t.= "<button class='btn btn-warning btn-sm' type='button' onclick='cancela_previo()'><i class='far fa-calendar-times'></i>Cancelar cita</button>";
+								$t.="</div>";
+							$t.="</div>";
+						$t.="</div>";
+					$t.="</div>";
+				$t.="</div>";
+				$arreglo=array('activo'=>1, 'dato'=>$x, 'tipo'=>$tipo, 'texto'=>$t);
+			}
+			else{
+				$arreglo=array('activo'=>0, 'dato'=>"0", 'tipo'=>$tipo);
+			}
+			return json_encode($arreglo);
+		}
+		catch(PDOException $e){
+			return "Database access FAILED!".$e->getMessage();
+		}
+	}
+	public function confirmar(){
+		try{
+			$cita=$_REQUEST['cita'];
+			$observaciones=$_REQUEST['observaciones'];
+			$actual=date('Y-m-d H:i:s');
+			$arreglo=array();
+			$arreglo+=array('idfolio'=>$_SESSION['idfolio']);
+			$arreglo+=array('fcreado'=>$actual);
+			$arreglo+=array('apartado'=>2);
+			$arreglo+=array('observaciones'=>$observaciones);
+			$x=$this->update('citas',array('id'=>$cita), $arreglo);
+			if($x){
+				$arreglo=array('id'=>0,'error'=>0, 'terror'=>"");
+			}
+			else{
+				$arreglo=array('id'=>0,'error'=>1, 'terror'=>$x);
+			}
+			return json_encode($arreglo);
+		}
+		catch(PDOException $e){
+			return "Database access FAILED!".$e->getMessage();
+		}
+	}
+	public function citas_lista2($tipo){
+		try{
+			self::set_names();
+			$fecha=date('Y-m-d')." 00:00:00";
+			$sql="select * from citas left outer join afiliados on afiliados.idfolio=citas.idfolio
+			where apartado=2 and tipo='$tipo' and fecha>='$fecha'";
+			$sth = $this->dbh->prepare($sql);
+			$sth->execute();
+			return $sth->fetchAll();
+		}
+		catch(PDOException $e){
+			return "Database access FAILED! ".$e->getMessage();
+		}
+	}
+	public function cita_ver($id){
+		try{
+			self::set_names();
+			$fecha=date('Y-m-d')." 00:00:00";
+			$sql="select citas.*, afiliados.nombre, afiliados.ape_pat, afiliados.ape_mat, afiliados.Filiacion from citas left outer join afiliados on afiliados.idfolio=citas.idfolio
+			where citas.id='$id'";
+			$sth = $this->dbh->prepare($sql);
+			$sth->execute();
+			return $sth->fetch(PDO::FETCH_OBJ);
+		}
+		catch(PDOException $e){
+			return "Database access FAILED! ".$e->getMessage();
+		}
+	}
+	public function citas_afiliados(){
+		try{
+			self::set_names();
+			$fecha=date('Y-m-d')." 00:00:00";
+			$sql="select * from citas where idfolio=:idfolio and apartado=2 and fecha>='$fecha' order by fecha desc";
+			$sth = $this->dbh->prepare($sql);
+			$sth->bindValue(":idfolio",$_SESSION['idfolio']);
+			$sth->execute();
+			return $sth->fetchAll(PDO::FETCH_OBJ);
+		}
+		catch(PDOException $e){
+			return "Database access FAILED! ".$e->getMessage();
+		}
+	}
+	public function cancelar_cita(){
+		$cita=$_REQUEST['cita'];
+		return $this->borrar("citas","id",$cita);
+	}
+}
+
+$db = new Escritorio();
+if(strlen($function)>0){
+  echo $db->$function();
+}
+
+
+?>
