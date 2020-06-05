8ca329960577ee09a9225d2045f1360be5e6190f
ericesponda <51395158+ericesponda@users.noreply.github.com>
2020-06-05 03:16:15 -0500

    x
    x

--- dash/index.php
+++ dash/index.php
@@ -19,6 +19,14 @@
 			<a class="btn btn-outline-primary btn-sm" id='menu_ventas' href='#a_citas/index'>Ir</a>
 		</div>
 
+		<div class="card bg-white text-black col-3" >
+			 <img class="card-img-top mx-auto d-block" src="img\pies.png" alt="Card image" style="width:100px">
+			<h5 class="card-title text-center">PRODUCTOS</h5>
+			<p class="card-text text-center">Registro de productos.</p>
+
+			<a class="btn btn-outline-primary btn-sm" id='menu_ventas' href='#a_productos/index'>Ir</a>
+		</div>
+
 
 	</div>
 </div>
